e20c4b9dfb2f17a532a6c3e511e403c845f86976
Matej Tyc <matyc@redhat.com>
2022-10-21 17:15:58 +0200

    Adjust after tests

--- app/history.py
+++ app/history.py
@@ -31,7 +31,10 @@ class Event:
     def __init__(self, time):
         self.time = time
         self.value = None
-        
+
+    def __str__(self):
+        return f"{self.time.date()}: {self.value}"
+
 
 class Timeline:
     _data: np.array
@@ -42,11 +45,11 @@ class Timeline:
         self.start = start
         period = end - start
         self._data = np.zeros(period.days + 1)
-        
+
     def recreate_with_value(self, value, dtype=np.float):
         self._data = np.empty_like(self._data, dtype=dtype)
         self._data[:] = value
-        
+
     @property
     def days(self):
         return len(self._data)
@@ -63,17 +66,17 @@ class Timeline:
     def set_value_at(self, time: datetime.datetime, value):
         index = localize_event(self.start, time)
         self._data[index] = value
-    
+
     def value_at(self, time: datetime.datetime):
         index = localize_event(self.start, time)
         return self._data[index]
-    
+
     def get_value_mask(self, value):
         return self._data == value
 
     def get_masked_values(self, mask):
         return self._data[mask]
-    
+
 
 class State(enum.IntEnum):
     unknown = enum.auto()
@@ -89,25 +92,25 @@ class Repre:
     points_timeline: Timeline
     status_timeline: Timeline
     time_timeline: Timeline
-    
+
     def __init__(self, start, end):
         self.points_timeline = Timeline(start, end)
         self.status_timeline = Timeline(start, end)
         self.status_timeline.recreate_with_value(State.unknown)
         self.time_timeline = Timeline(start, end)
-    
+
     def update(self, when, status=None, points=None, time=None):
         if points is not None:
             self.points_timeline.set_value_at(when, points)
         if status is not None:
             self.status_timeline.set_value_at(when, status)
-    
+
     def get_points_at(self, when):
         return self.points_timeline.value_at(when)
-    
+
     def get_status_at(self, when):
         return self.status_timeline.value_at(when)
-    
+
     def status_is(self, status: State):
         return self.status_timeline.get_value_mask(status)
 
@@ -123,33 +126,33 @@ class Repre:
 
         init_event.value = self.status_timeline.value_at(when)
         self.status_timeline.process_events([init_event])
-        
+
 
 class Aggregation:
     repres: typing.List[Repre]
-    
+
     def __init__(self):
         self.repres = []
 
     def add_repre(self, repre):
         self.repres.append(repre)
-    
+
     def states_on(self, when):
         states = set()
         for r in self.repres:
             states.add(r.get_status_at(when))
         return states
-    
+
     def points_on(self, when):
         points = 0.0
         for r in self.repres:
             points += r.get_points_at(when)
         return points
-    
+
     @property
     def days(self):
         return self.repres[0].points_timeline.days
-    
+
 
 class MPLPlot:
     def __init__(self, a: Aggregation):
@@ -160,32 +163,32 @@ class MPLPlot:
             (State.in_progress, empty_array.copy(), "grey"),
             (State.review, empty_array.copy(), "yellow"),
         ]
-        
+
     def _prepare_plots(self, up_to_date):
         for status, dest, color in self.styles:
             for r in self.aggregation.repres:
                 dest[r.status_is(status)] += r.points_of_status(status)
-            
+
     def _plot_bars(self, ax):
-        width = 0.5
+        width = 1.0
         days = np.arange(self.aggregation.days)
         bottom = np.zeros_like(days, dtype=np.float)
         for status, array, color in self.styles:
             ax.bar(days, array, width, bottom=bottom, label=status, color=color)
             bottom += array
-            
+
         ax.plot([days[0], days[-1]], [bottom[0], 0], color="blue")
-            
+
     def plot_stuff(self, up_to_date):
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots()
-        
+
         self._prepare_plots(up_to_date)
         self._plot_bars(ax)
         ax.legend()
 
         plt.show()
-        
+
 
 def demo_plot():
     start = datetime.datetime(2022, 10, 1)
@@ -210,6 +213,6 @@ def demo_plot():
     aggregation = Aggregation()
     aggregation.add_repre(repre)
     aggregation.add_repre(repre2)
-    
+
     plotter = MPLPlot(aggregation)
     plotter.plot_stuff(someday)
