b660738ae69a7cc8aea128493c98abb19e71e9b7
Matěj Týč <matej.tyc@gmail.com>
2023-02-08 01:37:16 +0100

    Pitch to fix next_url with Google login

--- estimage/webapp/main/google_login.py
+++ estimage/webapp/main/google_login.py
@@ -2,6 +2,7 @@ import flask
 import flask_login
 import requests
 import json
+import urllib.parse
 
 from oauthlib import oauth2
 
@@ -20,8 +21,18 @@ def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL, timeout=10).json()
 
 
+def encode_data_urlsafe(** kwargs):
+    unsafe_str = json.dumps(data_as_dict)
+    return urllib.parse.quote_plus(unsafe_str)
+
+
+def decode_urlsafe_data(safe_string):
+    dumped = urllib.parse.unquote_plus(safe_string)
+    return json.loads(dumped)
+
+
 @bp.route('/google_login')
-def google_login():
+def google_login(safe_next_page):
     form = forms.GoogleLoginForm()
     if form.validate_on_submit():
         google_provider_cfg = get_google_provider_cfg()
@@ -32,12 +43,14 @@ def google_login():
             authorization_endpoint,
             redirect_uri=flask.request.base_url + "/callback",
             scope=["openid", "email"],
+            state=encode_data_urlsafe(safe_next_page=safe_next_page)
         )
         return flask.redirect(request_uri)
 
     login_provider = flask.current_app.config["LOGIN_PROVIDER_NAME"]
     return flask.render_template(
-        'login.html', title='Sign In', login_form=form, login_provider=login_provider)
+        'login.html', title='Sign In', login_form=form,
+        next=safe_next_page, login_provider=login_provider)
 
 
 @bp.route('/login/callback', methods=['GET', 'POST'])
@@ -48,6 +61,8 @@ def google_callback_dest():
 
     google_auth_code = flask.request.args.get("code")
     google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+    state_object = decode_urlsafe_data(flask.request.args.get("state"))
     token_endpoint = google_provider_cfg["token_endpoint"]
 
     token_url, headers, body = client.prepare_token_request(
@@ -72,7 +87,7 @@ def google_callback_dest():
     user, domain = mail.split("@", 1)
     user = User(user, domain)
     flask_login.login_user(user)
-    return flask.redirect(flask.url_for('main.tree_view'))
+    return flask.redirect(state_object["safe_next_page"])
 
 
 def _get_user_email(client):
--- estimage/webapp/main/routes.py
+++ estimage/webapp/main/routes.py
@@ -51,29 +51,31 @@ def logout():
     return flask.redirect(flask.url_for("main.login"))
 
 
-def autologin():
+def autologin(safe_next_page):
     form = forms.AutoLoginForm()
     if form.validate_on_submit():
         user = User(form.username.data)
         flask_login.login_user(user, remember=form.remember_me.data)
-
-        next_page = flask.request.args.get('next')
-        if not next_page or werkzeug.urls.url_parse(next_page).netloc != '':
-            next_page = flask.url_for('main.tree_view')
-        return flask.redirect(next_page)
+        return flask.redirect(safe_next_page)
     login_provider = flask.current_app.config["LOGIN_PROVIDER_NAME"]
     return render_template(
-        'login.html', title='Sign In', login_form=form, login_provider=login_provider)
+        'login.html', title='Sign In', login_form=form,
+        next=safe_next_page, login_provider=login_provider)
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     config_dict = flask.current_app.config
+
+    next_page = flask.request.args.get('next')
+    if not next_page or werkzeug.urls.url_parse(next_page).netloc != '':
+        next_page = flask.url_for('main.tree_view')
+
     match provider_name := config_dict["LOGIN_PROVIDER_NAME"]:
         case "autologin":
-            return autologin()
+            return autologin(next_page)
         case "google":
-            return google_login()
+            return google_login(next_page)
         case _:
             msg = f"Unknown login provider: {provider_name}"
             raise ValueError(msg)
