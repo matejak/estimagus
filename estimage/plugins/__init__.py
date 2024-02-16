ed20ad2a2efe4771519f34da1fc1d5c29c9f7165
Matěj Týč <matyc@redhat.com>
2024-02-16 17:50:00 +0100

    Implement better handling of plugin-related exceptions

--- estimage/plugins/__init__.py
+++ estimage/plugins/__init__.py
@@ -1,12 +1,19 @@
 import importlib
+import logging
 
 
 def _get_plugin_routes_module(plugin):
     if not hasattr(plugin, "routes"):
+        full_module_name = f"{plugin.__name__}.routes"
         try:
-            importlib.import_module(f"{plugin.__name__}.routes")
-        except Exception:
-            return None
+            importlib.import_module(full_module_name)
+        except ModuleNotFoundError as exc:
+            if exc.name == full_module_name:
+                msg = f"Plugin {plugin.__name__} doesn't have the routes module"
+                logging.error(msg)
+                return None
+            msg = f"Couldn't import '{full_module_name}' due to a missing dependency '{exc.name}'"
+            raise RuntimeError(msg)
     return plugin.routes
 
 
