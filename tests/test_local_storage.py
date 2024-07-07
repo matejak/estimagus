fd218ac67b7ba5d34c2b85429322afd741d8f0f9
Matěj Týč <matej.tyc@gmail.com>
2024-07-08 01:21:32 +0200

    Add an ini backend

--- tests/test_local_storage.py
+++ tests/test_local_storage.py
@@ -1,17 +1,28 @@
 import pytest
 
 import estimage.local_storage as tm
+import estimage.persistence.storage.ini as tm_ini
+
+from tests.test_inidata import temp_filename
 
 
 class MemoryIO:
     def __init__(self):
         self.storage = dict()
+        self.storage[tuple()] = dict()
 
     def save_dict(self, data):
         self.storage.update(data)
 
-    def load_to_dict(self, keys):
-        return {key: self.storage[key] for key in keys}
+    def load_keys_to_dict(self, keys):
+        return {key: self.storage[tuple()][key] for key in keys}
+
+    def load_ns_to_dict(self, nsname):
+        return {nsname: self.storage[nsname]}
+
+
+class IniIO(tm_ini.IniStorageIO):
+    pass
 
 
 def test_stores():
@@ -43,12 +54,14 @@ def test_is_isolated():
         assert storage.get_key("key")
 
 
-@pytest.fixture(params=("memory",))
-def storage_io(request):
+@pytest.fixture(params=("memory", "ini"))
+def storage_io(request, temp_filename):
     choices = dict(
         memory=MemoryIO,
+        ini=IniIO,
     )
     io = choices[request.param]()
+    io.CONFIG_FILENAME = temp_filename
     yield io
 
 
@@ -70,18 +83,39 @@ def test_persistence(storage_io):
     assert storage_out.get_key("key") == "value"
 
 
-def test_namespaces(storage_io):
+def test_ns_key_exists(storage_io):
+    storage_in = tm.Storage()
+    storage_in.feed_key("key", "value", namespace=("ns",))
+    storage_in.feed_key("kez", "valuf", namespace=("first", "second"))
+    storage_in.save(storage_io)
+
+    storage_out = tm.Storage()
+    storage_out.request_namespace(("ns",))
+    storage_out.request_namespace(("first", "second"))
+    storage_out.load(storage_io)
+    assert storage_out.get_namespace(("ns",))["key"] == "value"
+    assert storage_out.get_namespace(("first", "second"))["kez"] == "valuf"
+
+
+def test_nonexistent_ns_empty_noerror(storage_io):
     storage_in = tm.Storage()
     storage_in.feed_key("key", "value", namespace=("ns",))
     storage_in.save(storage_io)
 
     storage_out = tm.Storage()
-    storage_out.request_namespace(("ns",))
-    storage_out.load(storage_io)
-    assert storage_out.get_namespace(("ns",))["key"] == "value"
-    storage_out.request_namespace(("one", "two"))
     storage_out.request_namespace(("one", "one"))
+    storage_out.request_namespace(("ns", "one"))
     storage_out.load(storage_io)
     assert not storage_out.get_namespace(("one", "one"))
-    assert storage_out.get_namespace(("one", "two"))["key"] == "value"
+    assert not storage_out.get_namespace(("ns", "one"))
 
+
+def test_nonns_key_doesnt_exist(storage_io):
+    storage_in = tm.Storage()
+    storage_in.feed_key("key", "value", namespace=("ns",))
+    storage_in.save(storage_io)
+
+    storage_out = tm.Storage()
+    storage_out.request_key("key")
+    with pytest.raises(KeyError, match="key"):
+        storage_out.load(storage_io)
