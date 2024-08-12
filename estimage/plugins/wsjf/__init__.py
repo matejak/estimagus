f9b5d8bc4ee5ea451953b42405fd1b8046afbc88
Matěj Týč <matyc@redhat.com>
2024-08-12 17:59:31 +0200

    Expand the wsjf model

--- estimage/plugins/wsjf/__init__.py
+++ estimage/plugins/wsjf/__init__.py
@@ -4,10 +4,30 @@ from ... import persistence
 class WSJFCard:
     business_value: float = 0
     risk_and_opportunity: float = 0
+    time_sensitivity: float = 0
+    inherited_priority: dict
+
+    def __init__(self, * args, **kwargs):
+        super().__init__(* args, ** kwargs)
+        self.inherited_priority = dict()
 
     @property
     def cost_of_delay(self):
-        return self.business_value + self.risk_and_opportunity
+        ret = (
+            self.business_value
+            + self.risk_and_opportunity
+            + self.time_sensitivity)
+        ret += sum(self.inherited_priority.values()) * self.point_cost
+        return ret
+
+    @property
+    def wsjf_score(self):
+        if self.cost_of_delay == 0:
+            return 0
+        if self.point_cost == 0:
+            msg = f"Point Cost aka size of '{self.name}' is unknown, as is its priority."
+            raise ValueError(msg)
+        return self.cost_of_delay / self.point_cost
 
     def pass_data_to_saver(self, saver):
         super().pass_data_to_saver(saver)
@@ -23,6 +43,12 @@ class IniCardStateLoader:
     def load_wsjf_fields(self, card):
         card.business_value = float(self._get_our(card, "wsjf_business_value"))
         card.risk_and_opportunity = float(self._get_our(card, "wsjf_risk_and_opportunity"))
+        card.time_sensitivity = float(self._get_our(card, "time_sensitivity"))
+
+        records = self._get_our(card, "inherited_priority")
+        for record in records.split(";"):
+            source, value = record.split(",")
+            card.inherited_priority[source] = float(value)
 
 
 @persistence.saver_of(WSJFCard, "ini")
@@ -30,3 +56,9 @@ class IniCardStateSaver:
     def save_wsjf_fields(self, card):
         self._store_our(card, "wsjf_business_value", str(card.business_value))
         self._store_our(card, "wsjf_risk_and_opportunity", str(card.risk_and_opportunity))
+        self._store_our(card, "time_sensitivity", str(card.time_sensitivity))
+
+        record = []
+        for source, value in card.inherited_priority.items():
+            record.append(f"{source},{value}")
+        self._store_our(card, "inherited_priority", ";".join(record))
--- estimage/plugins/wsjf/tests/test_wsjf.py
+++ estimage/plugins/wsjf/tests/test_wsjf.py
@@ -8,9 +8,18 @@ from tests.test_inidata import temp_filename, inifile_temploc, cardio_inifile_cl
 
 
 @pytest.fixture()
-def wsjf_card():
-    card = tm.WSJFCard()
-    return card
+def wsjf_cls():
+    resolver = PluginResolver()
+    resolver.add_known_extendable_classes()
+    assert "BaseCard" in resolver.class_dict
+    resolver.resolve_extension(tm, dict(BaseCard="WSJFCard"))
+    cls = resolver.class_dict["BaseCard"]
+    return cls
+
+
+@pytest.fixture()
+def wsjf_card(wsjf_cls):
+    return wsjf_cls("card")
 
 
 @pytest.fixture(params=("ini",))
@@ -28,24 +37,42 @@ def test_cod(wsjf_card):
     assert wsjf_card.cost_of_delay == 3
 
 
-def test_cod_with_dependencies():
-    pass
+def test_cod_with_dependencies(wsjf_card):
+    wsjf_fill(wsjf_card)
+    sum_of_own_fields = 15
+    assert wsjf_card.cost_of_delay >= sum_of_own_fields
+    wsjf_card.point_cost = 1
+    assert wsjf_card.cost_of_delay == sum_of_own_fields + 2
+    wsjf_card.point_cost = 2
+    assert wsjf_card.cost_of_delay == sum_of_own_fields + 2 * 2
 
 
-def test_persistence(card_io):
-    resolver = PluginResolver()
-    resolver.add_known_extendable_classes()
-    assert "BaseCard" in resolver.class_dict
-    resolver.resolve_extension(tm, dict(BaseCard="WSJFCard"))
-    cls = resolver.class_dict["BaseCard"]
-    base_card_load_save(card_io, cls, plugin_fill, plugin_test)
+def test_priority(wsjf_card):
+    assert wsjf_card.wsjf_score == 0
+    wsjf_fill(wsjf_card)
+    with pytest.raises(ValueError, match="size"):
+        wsjf_card.wsjf_score
+    wsjf_card.point_cost = 1
+    sum_of_own_fields = 15
+    assert wsjf_card.cost_of_delay == sum_of_own_fields + 2
+    wsjf_card.point_cost = 2
+    assert wsjf_card.wsjf_score == sum_of_own_fields / 2 + 2
+
+
+def test_persistence(card_io, wsjf_cls):
+    base_card_load_save(card_io, wsjf_cls, plugin_fill, plugin_test)
+
+
+def wsjf_fill(card):
+    card.business_value = 7
+    card.risk_and_opportunity = 3
+    card.time_sensitivity = 5
+    card.inherited_priority["one"] = 2
 
 
 def plugin_fill(card):
     fill_card_instance_with_stuff(card)
-
-    card.business_value = 7
-    card.risk_and_opportunity = 2.2
+    wsjf_fill(card)
 
 
 def plugin_test(lhs, rhs):
@@ -53,3 +80,5 @@ def plugin_test(lhs, rhs):
     assert lhs.business_value == rhs.business_value
     assert lhs.risk_and_opportunity == rhs.risk_and_opportunity
     assert lhs.cost_of_delay == rhs.cost_of_delay
+    assert lhs.time_sensitivity == rhs.time_sensitivity
+    assert lhs.inherited_priority["one"] == rhs.inherited_priority["one"]
