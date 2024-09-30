82c16f40e58a4c33a25b9ed378d735eb7d50d7ed
Matěj Týč <matej.tyc@gmail.com>
2024-09-30 21:27:39 +0200

    Consolidate mechanics of models

--- estimage/entities/model.py
+++ estimage/entities/model.py
@@ -6,7 +6,7 @@ from .composition import Composition
 from .card import BaseCard
 
 
-class EstiModel:
+class Model:
     name_result_map: typing.Mapping[str, TaskModel]
     name_composition_map: typing.Mapping[str, Composition]
     main_composition: Composition
@@ -41,18 +41,48 @@ class EstiModel:
     def _get_main_composition(self):
         return Composition("")
 
+    # TODO: strange
     def _get_new_element(self, name):
         return TaskModel(name)
 
+    # TODO: strange - used in tests
     def new_element(self, name: str):
         e = self._get_new_element(name)
         self.add_element(e)
 
+    # TODO: strange, why to add to the main composition?
     def add_element(self, element):
         if (name := element.name) in self.name_result_map:
             raise RuntimeError(f"Already encountered element of name {name}")
         self.name_result_map[name] = element
         return self.main_composition.add_element(element)
+
+    def get_element(self, name):
+        return self.name_result_map[name]
+
+    def update_cards_with_values(self, cards: typing.Container[BaseCard]):
+        for t in cards:
+            self._update_card(t)
+
+    def _update_card(self, card: BaseCard):
+        for dep in card.children:
+            self._update_card(dep)
+        if card.name not in self.name_result_map:
+            return
+        element = self.name_result_map[card.name]
+        self._update_card_by_result(card, element)
+
+    def _update_card_by_result(self, card: BaseCard, element: TaskModel):
+        pass
+
+    # TODO: Used only in tests - redo the tests to use card update - problematic type-wise
+    def export_element(self, name: str) -> BaseCard:
+        card = BaseCard(name)
+        self._update_card(card)
+        return card
+
+
+class EstiModel(Model):
 
     @property
     def nominal_point_estimate(self):
@@ -97,23 +127,7 @@ class EstiModel:
         element = self.name_result_map[name]
         element.nullify()
 
-    def get_element(self, name):
-        return self.name_result_map[name]
-
-    def update_cards_with_values(self, cards: typing.Container[BaseCard]):
-        for t in cards:
-            self._update_card(t)
-
-    def _update_card(self, card: BaseCard):
-        for dep in card.children:
-            self._update_card(dep)
-        if card.name not in self.name_result_map:
-            return
-        element = self.name_result_map[card.name]
+    def _update_card_by_result(self, card: BaseCard, element: TaskModel):
+        super()._update_card_by_result(card, element)
         card.point_cost = element.nominal_point_estimate.expected
         card.time_cost = element.nominal_time_estimate.expected
-
-    def export_element(self, name: str) -> BaseCard:
-        card = BaseCard(name)
-        self._update_card(card)
-        return card
